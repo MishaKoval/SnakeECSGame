2feaff614a3d55bea409250f2257c59965bc93c8
MiKo <kovalevsky.m@razum.by>
2023-07-26 09:42:07 +0300

    Change webSockets to native

--- Assets/Project/Utilities/WebSocketTest.cs
+++ Assets/Project/Utilities/WebSocketTest.cs
@@ -4,16 +4,5 @@ namespace Project.Utilities
 {
     public class WebSocketTest : MonoBehaviour
     {
-        [ContextMenu("TESTCreate")]
-        private async void TestCreate()
-        {
-            Debug.Log(await WebSocketUtility.TestCreateGame());
-        }
-        
-        [ContextMenu("TESTEnd")]
-        private async void TestEnd()
-        {
-            Debug.Log(await WebSocketUtility.TestEndGame());
-        }
     }
 }
